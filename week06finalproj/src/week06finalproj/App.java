8ff2586cd6f31523ddc0bc806eddd7e09067be57
cwahlund <wahlund@icloud.com>
2023-06-06 14:36:43 -0700

    Moved some items to methods to simplify game logic section

--- week06finalproj/src/week06finalproj/App.java
+++ week06finalproj/src/week06finalproj/App.java
@@ -10,23 +10,25 @@ public class App {
 		Player player2 = new Player("Player Two");
 		int cardsAtStake = 2;
 		
-		playDeck.shuffle();
 		
 		// WAR is played 26 times for two players with a total of 52 cards
-		for (int i = 1; i <=26; i++) {
+		int hands = 26;
+		
+		playDeck.shuffle();
+
+		// game logic
+		for (int i = 1; i <=hands ; i++) {
 			player1.setCurrentCard(playDeck.draw());
 			player2.setCurrentCard(playDeck.draw());
 
-			
+			// Watch for deck being empty, this should not happen in 2 player WAR, but is there for safety or expanding
 			if (player1.getCurrentCard() == null || player2.getCurrentCard() == null) {
 				break;
 			}
 			
-			System.out.print(player1.getName() + "'s card: ");
-			player1.getCurrentCard().describe();
-			System.out.print(player2.getName() + "'s card: ");
-			player2.getCurrentCard().describe();
+			ShowCards(player1, player2);
 			
+			// calculate winner of hand
 			if (player1.getCurrentCard().getValue() > player2.getCurrentCard().getValue()) {
 				player1.setScore(player1.getScore() + cardsAtStake);
 				System.out.println(player1.getName() + " won this hand.");
@@ -36,7 +38,7 @@ public class App {
 				System.out.println(player2.getName() + " won this hand.");
 				cardsAtStake = 2;
 			} else {
-				// play another hand, the stakes go up by two cards
+				// if a tie play another hand, the stakes go up by two cards
 				System.out.println("WAR!");
 				cardsAtStake += 2;
 				i++;
@@ -45,6 +47,18 @@ public class App {
 			System.out.println("-------------------------");
 		}
 		
+		PrintFinalScore(player1, player2);
+	}
+	
+	private static void ShowCards(Player player1, Player player2) {
+		System.out.print(player1.getName() + "'s card: ");
+		player1.getCurrentCard().describe();
+		System.out.print(player2.getName() + "'s card: ");
+		player2.getCurrentCard().describe();
+	}
+
+	
+	private static void PrintFinalScore(Player player1, Player player2) {
 		System.out.println(player1.getName() + " won " + player1.getScore() + " cards.");
 		System.out.println(player2.getName() + " won " + player2.getScore() + " cards.");
 		
@@ -56,5 +70,4 @@ public class App {
 			System.out.println("The game was a draw.");
 		}
 	}
-
 }
